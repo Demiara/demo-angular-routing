4290fadfa360a5f937bbe4a28a8543035fea93ef
Tatyana Kutalo <tatyana@kutalo.com>
2020-07-20 18:53:12 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { FormsModule } from '@angular/forms';
+import { ComposeMessageComponent } from './compose-message/compose-message.component';
+
+import { AuthModule } from './auth/auth.module';
+import { Router } from '@angular/router';
+
+@NgModule({
+    declarations: [AppComponent, PageNotFoundComponent, ComposeMessageComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HeroesModule,
+        AuthModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+})
+export class AppModule {
+    // Diagnostic only: inspect router configuration
+    constructor(router: Router) {
+        // Use a custom replacer to display function names in the route configs
+        const replacer = (key, value) => (typeof value === 'function' ? value.name : value);
+        console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+    }
+}
