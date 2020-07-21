deb4ed8e943988780f45a89d567742ee8f191d8f
Tatyana Kutalo <tatyana@kutalo.com>
2020-07-21 20:48:30 +0300

    Adding PageNotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HeroesModule } from './heroes/heroes.module';
 import { NgModule } from '@angular/core';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, PageNotFoundComponent],
     imports: [BrowserModule, FormsModule, HeroesModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
