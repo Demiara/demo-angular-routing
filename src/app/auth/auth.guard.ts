494327ce0d8f7890c2049c08b51b5e56b53aa988
Tatyana Kutalo <tatyana@kutalo.com>
2020-07-23 12:42:17 +0300

    AuthGuard redirect bug fix

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -30,7 +30,7 @@ export class AuthGuard implements CanActivate {
         if (this.authService.isLoggedIn) {
             return true;
         }
-
+        this.authService.redirectUrl = url;
         this.router.navigate(['/login']);
         return false;
     }
